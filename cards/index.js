2e22718943456e621f576e330e527f8e64a19f94
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-02-26 19:34:35 +0300

    Append card from js

--- cards/index.js
+++ cards/index.js
@@ -1,6 +1,28 @@
 const btn = document.querySelector(".btn")
+const cardContainer = document.querySelector(".card-container")
+
+const card = document.createElement('div')
+const cardFrontSide = document.createElement('div')
+const cardBackSide = document.createElement('div')
+
+card.className = 'card';
+cardFrontSide.className = 'card-side card-front'
+cardBackSide.className = 'card-side card-back'
+
+const frontParagpaph = document.createElement('p')
+frontParagpaph.innerHTML = 'front'
+
+const backParagpaph = document.createElement('p')
+backParagpaph.innerHTML = 'back'
+
+cardFrontSide.appendChild(frontParagpaph)
+cardBackSide.appendChild(backParagpaph)
+
+card.appendChild(cardFrontSide)
+card.appendChild(cardBackSide)
 
 btn.addEventListener('click', function(e){
     e.preventDefault()
-    console.log('click')
+    cardContainer.appendChild(card)
+    console.log(cardContainer)
 })
