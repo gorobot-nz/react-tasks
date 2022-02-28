7f23c5c7a20d143fcf80e4fee6f39b8ca03580de
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-03-01 00:43:13 +0300

    Fix rotation

--- cards/js/main.js
+++ cards/js/main.js
@@ -1,36 +1,45 @@
 const cards = [];
+const cardsMap = new Map();
+let counter = 0;
 
 const FIRST = 'first'
 const LAST = 'last'
 
 const addFirstbutton = document.querySelector("#insert-first")
+const shufflebutton = document.querySelector("#shuffle")
 const addLastbutton = document.querySelector("#insert-last")
 const cardContainer = document.querySelector(".card-container")
 
 class Card {
-    constructor(text, color){
+
+    constructor(text, color) {
         this.text = text
         this.color = color
+        this.id = counter;
+        counter++;
     }
 
-    setCardSideStyles(element, name){
+    setCardSideStyles(element, name) {
         element.className = name
         element.style.background = this.color
     }
 
-    getDomCard(){
+    getDomCard() {
         const card = document.createElement('div')
         const cardFrontSide = document.createElement('div')
         const cardBackSide = document.createElement('div')
 
         card.className = 'card';
-        card.id = cards.length;
         this.setCardSideStyles(cardFrontSide, 'card-side card-front')
         this.setCardSideStyles(cardBackSide, 'card-side card-back')
+
+        const frontParagraph = document.createElement('p')
+        frontParagraph.innerHTML = this.id
 
         const backParagpaph = document.createElement('p')
         backParagpaph.innerHTML = this.text
 
+        cardFrontSide.appendChild(frontParagraph)
         cardBackSide.appendChild(backParagpaph)
 
         card.appendChild(cardFrontSide)
@@ -40,70 +49,89 @@ class Card {
     }
 }
 
-function getRandomColor() {       
-    const letters = '0123456789ABCDEF'.split('');       
-    let color = '#';       
-    
-    for (var i = 0; i < 6; i++) {          
-        color += letters[Math.floor(Math.random() * letters.length)];       
-    }        
-    
-    return color;     
-}
-    
-function getRandomSentence(length) {       
-    const words = ["The sky", "above", "the port", "was", "the color of television", "tuned", "to", "a dead channel", ".", "All", "this happened", "more or less", ".", "I", "had", "the story", "bit by bit", "from various people", "and", "as generally", "happens", "in such cases", "each time", "it", "was", "a different story", ".", "It", "was", "a pleasure", "to", "burn"];        
-    
-    let sentence = "";        
-    
-    for (let i = 0; i < length; i++) {         
-        sentence = `${sentence} ${words[Math.floor(Math.random() * words.length)]}`;      
-    }        
-    
-    return sentence;    
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
 }
 
-function createGradient(){
+function getRandomColor() {
+    const letters = '0123456789ABCDEF'.split('');
+    let color = '#';
+
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * letters.length)];
+    }
+
+    return color;
+}
+
+function getRandomSentence(length) {
+    const words = ["The sky", "above", "the port", "was", "the color of television", "tuned", "to", "a dead channel", ".", "All", "this happened", "more or less", ".", "I", "had", "the story", "bit by bit", "from various people", "and", "as generally", "happens", "in such cases", "each time", "it", "was", "a different story", ".", "It", "was", "a pleasure", "to", "burn"];
+
+    let sentence = "";
+
+    for (let i = 0; i < length; i++) {
+        sentence = `${sentence} ${words[Math.floor(Math.random() * words.length)]}`;
+    }
+
+    return sentence;
+}
+
+function createGradient() {
     let startColor = getRandomColor()
     let endColor = getRandomColor()
 
     return `linear-gradient(${startColor}, ${endColor})`
 }
 
-addFirstbutton.addEventListener('click', function(e) {
+addFirstbutton.addEventListener('click', function (e) {
     e.preventDefault()
     createCard(FIRST)
 })
 
-addLastbutton.addEventListener('click', function(e) {
+addLastbutton.addEventListener('click', function (e) {
     e.preventDefault()
     createCard(LAST)
 })
 
-function createCard (position){
-    if (cards.length == 20){
+shufflebutton.addEventListener('click', function (e) {
+    e.preventDefault()
+    shuffle(cards)
+    cardContainer.innerHTML = ''
+    cards.forEach(card => render(card, LAST))
+})
+
+function createCard(position) {
+    if (cards.length == 20) {
         alert("Enought cards")
         return
     }
 
-    let sentence = getRandomSentence(10)    
+    let sentence = getRandomSentence(10)
     let color = createGradient()
 
     const card = new Card(sentence, color)
-    addToList(card.getDomCard(), position)
+    addToList(card, position)
 }
 
-function addToList(node, position){
-    if (position === FIRST){
-        cards.push(node)
+function addToList(node, position) {
+    if (position === FIRST) {
+        cards.push(node.id)
     } else {
-        cards.unshift(node)
+        cards.unshift(node.id)
     }
-    render(node, position)
+    cardsMap.set(node.id, node.getDomCard())
+    render(node.id, position)
 }
 
-function render(node, position) {
-    if (position === FIRST){
+function render(id, position) {
+    const node = cardsMap.get(id)
+    if (position === FIRST) {
         cardContainer.insertBefore(node, cardContainer.firstChild)
     } else {
         cardContainer.appendChild(node)
