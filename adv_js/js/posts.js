c36af79213b20f8125a73cab23863ba5bd14fa63
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-03-02 13:57:59 +0300

    get users on window load

--- adv_js/js/posts.js
+++ adv_js/js/posts.js
@@ -1,10 +1,14 @@
 const posts = new Map()
 const users = new Map()
-const comments = new Map( )
+const comments = new Map()
 
 
 class User {
-
+    constructor(id, name, username) {
+        this.id = id
+        this.name = name
+        this.username = username
+    }
 }
 
 class Post {
@@ -17,11 +21,19 @@ class Post {
 }
 
 window.onload = async() => {
-    const responce = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const data = await responce.json()
-    data.forEach(item => {
+    const postsResponce = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const postsData = await postsResponce.json()
+    postsData.forEach(item => {
         const post = new Post(item.id, item.userId, item.title, item.body)
         posts.set(post.id, post)
     });
+    const usersResponce = await fetch('https://jsonplaceholder.typicode.com/users')
+    const usersData = await usersResponce.json()
+    usersData.forEach(item => {
+        const user = new User(item.id, item.name, item.username)
+        users.set(user.id, user)
+    });
+
     console.log(posts)
+    console.log(users)
 }
